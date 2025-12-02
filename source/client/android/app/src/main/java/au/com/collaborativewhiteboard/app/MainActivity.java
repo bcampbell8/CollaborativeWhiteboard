79f7c5b906a276a0aef29b9f27c0f125c6a30442
bcampbell <braeden.campbell2@studytafensw.edu.au>
2025-12-02 12:26:10 +1100

    Added vitest basic tests and directory for capacitor web app

---
+++ source/client/android/app/src/main/java/au/com/collaborativewhiteboard/app/MainActivity.java
@@ -0,0 +1,5 @@
+package au.com.collaborativewhiteboard.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ source/client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'au.com.collaborativewhiteboard.app',
+  appName: 'Collaborative Whiteboard',
+  webDir: 'build'
+};
+
+export default config;
