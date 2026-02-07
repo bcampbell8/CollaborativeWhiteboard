9a92d313b2a81f9ba565a58d4d3b9f5c0400794f
bcampbell8 <braeden.campbell8@gmail.com>
2026-02-07 19:52:25 +1100

    Added new button to Home.tsx to create rooms
    HostButton's purpose is to help facilitate refactoring of Host.tsx and Participant.tsx. Ideally, there will only be one view served to all participants, the Participant.tsx file, consolidating the code and thus removing the need for a separate Host.tsx file. Should there eventually be features added which involve separate UI for host, this can be added conditionally to the base Participant view.

---
+++ src/client/src/assets/HostButton.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import type Room from '../../../server/IWDB.ts'
+
+
+export type hostButtonProps = {
+    address: string
+}
+
+function HostButton(props: hostButtonProps){
+    const [room, setRoom] = useState<Room>();
+
+
+    async function handleClick(){
+        try {
+            const response = await fetch('http://192.168.0.2:2211/create');
+            const incRoom = await response.json();
+            console.log(incRoom);
+            setRoom(incRoom);
+            window.location.href = `/participate/${encodeURIComponent(incRoom._id)}`
+        } catch (error) {
+            console.log('Failed to create room. Error: ' + error);
+        }
+    }
+
+    return(
+        <>
+        <button onClick={handleClick}>Host Room</button>
+        </>
+    )
+
+}
+export default HostButton;
